823ff35353cbcabcf7b536dd2523335c44b7096c
AnantaViryadiva <99464924+AnantaViryadiva@users.noreply.github.com>
2022-04-18 13:53:17 +0700

    Add files via upload

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    nama: "Beyonce",
+    imgURL:
+      "https://assets.pikiran-rakyat.com/crop/14x1:777x449/x/photo/2021/07/23/3600883340.png",
+    telp: "+123 456 789",
+    email: "b@beyonce.com"
+  },
+  {
+    nama: "Jack Bauer",
+    imgURL:
+      "https://kids.kiddle.co/images/thumb/b/bc/Jack_Bauer1.jpg/300px-Jack_Bauer1.jpg",
+    telp: "+987 654 321",
+    email: "jack@nowhere.com"
+  },
+  {
+    nama: "Chuck Norris",
+    imgURL:
+      "https://ceritamengenai.files.wordpress.com/2019/10/kisah-cerita-chuck-norris.png?w=548",
+    telp: "+918 372 574",
+    email: "gmail@chucknorris.com"
+  }
+];
+
+export default contacts;
